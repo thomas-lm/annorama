9c87a9719ca0070df306a9c7f0cc2ceedfe01260
thomas.le-magourou <thomas.le-magourou@kosmos.fr>
2019-09-09 17:25:57 +0200

    problem lancement

--- src/main/index.js
+++ src/main/index.js
@@ -1,16 +1,16 @@
 'use strict'
 
 import { app, BrowserWindow, ipcMain } from 'electron'
+// import { getAppData, saveAppData } from '../renderer/store/storage.js'
+
 import '../renderer/store'
 import path from 'path'
-import { getAppData, saveAppData } from '../storage.js'
-
 /**
  * Set `__static` path to static files in production
  * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-static-assets.html
  */
 if (process.env.NODE_ENV !== 'development') {
-  global.__static = require('path').join(__dirname, '/static').replace(/\\/g, '\\\\')
+  global.__static = path.join(__dirname, '/static').replace(/\\/g, '\\\\')
 }
 
 let mainWindow, parserWindow
@@ -18,6 +18,14 @@ let mainWindow, parserWindow
 const winURL = process.env.NODE_ENV === 'development'
   ? `http://localhost:9080`
   : `file://${__dirname}/index.html`
+
+function getAppData (string) {
+  console.log('getData ', string)
+}
+
+function saveAppData (string, string1) {
+  console.log('saveData ', string, string1)
+}
 
 function createWindow () {
   /**
